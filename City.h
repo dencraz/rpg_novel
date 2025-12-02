59513584e00e9b7fc3edf89d9567a964df74ebcb
dencraz <daniil2007332@gmail.com>
2025-12-03 00:26:35 +0300

    Add files via upload

--- City.h
+++ City.h
@@ -1,26 +1,31 @@
 #pragma once
-
 #include <string>
 
-class LocationMenu; 
+class LocationMenu;
+class Player;
+class MainMenu;
 
-class City {
+class City{
 private:
     LocationMenu* locationMenuPtr;
+    Player* playerPtr;
+    MainMenu* mainMenuPtr;
+    static bool minerTalkedAboutBoss; 
 
-   
     void visitHome();
     void visitLibrary();
     void visitTavern();
+    void visitShop();
+    void talkToMiner(); // Новый метод  
     bool diceGame();
 
 public:
     City();
+    ~City();
 
     void setLocationMenu(LocationMenu* locMenu);
+    void setPlayer(Player* player);
     void enter();
     std::string getDescription() const;
+    static bool hasMinerUnlockedBoss() { return minerTalkedAboutBoss; }
 };
-
-
-
